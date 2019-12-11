0de8e946676c2daec4c77cd024c6c3cc3dc01589
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-11 01:53:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,8 @@
 veg1 = "broccolli"
 veg2 = "macaroni"
 
-def meal_choice(protein = "meat", veg1, veg2)
+def meal_choice(protein, veg1, veg2)
+  protein = "meat"
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
